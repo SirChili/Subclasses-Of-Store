34ef77489d89a37991f4eac653275f2b6890c6ca
SirChili <thomastony4628@gmail.com>
2025-09-25 18:19:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "Store.h";
 
+
 using namespace std;
 
 int main() {
@@ -9,27 +10,47 @@ int main() {
 	WebStore item1;
 	Store item2;
 	PhysicalStore item3;
+	GroceryStore item4;
 
+	//yes or no variable options
 	string DT = "Yes";
 	string DF = "No";
+	
 
-	item1 = WebStore("8.8.8.8", false);
-	item2 = Store("Walmart", 19245.6);
-	item3 = PhysicalStore("1845 Fake Address Rd 49538", true);
+	//change variables here
+	item1 = WebStore("The Data Store","www.thedatastore.com", false, 345.99); 
+	item2 = Store("The Everything Store", 123.99);
+	item3 = PhysicalStore("TheShoeStore","1845 Fake Address Rd 49538",254.76, true);
+	item4 = GroceryStore("The Grocery Store",8.94,"42 Faker Address 7th Ave.",false,true, true);
 
+	//----------------------------------------------------------------
+	//Store
+	cout << "Name: " << item2.getStoreName() << "\n";
+	cout << "Revenue: $" << item2.getStoreRevenue() << "\n\n";
+
+	//----------------------------------------------------------------
 	//Web store
-	cout << "Web Store\n";
-	cout << "Web IP Address: " << item1.getWebAddress() << "\n";
-	if (item1.getDroneDeliveryBool() == 0){
+	cout << "Web Store" << "\n";
+	cout << "Name: " << item1.getStoreName() << "\n";
+	cout << "Revenue: " << item1.getStoreRevenue() << "\n";
+	cout << "Web Address: " << item1.getWebAddress() << "\n";
+	
+	//display yes or no instead of 0 or 1
+	if (item1.getDroneDeliveryBool() == 0) {
 		cout << "Does Drone Delivery: No\n\n";
 	}
 	else {
 		cout << "Does Drone Delivery: Yes\n\n";
 	}
 
+	//----------------------------------------------------------------
 	//Physical Store
-	cout << "Physical Store\n";
-	cout << "Physical Store Address: " << item3.getPhysicalAddress() << "\n";
+	cout << "Physical Store" << "\n";
+	cout << "Name: " << item3.getStoreName() << "\n";
+	cout << "Revenue: " << item3.getStoreRevenue() << "\n";
+	cout << "Physical Address: " << item3.getPhysicalAddress() << "\n";
+	
+	//display yes or no instead of 0 or 1
 	if (item3.getHasWifi() == 0) {
 		cout << "Does Store has WiFi: No\n\n";
 	}
@@ -37,12 +58,34 @@ int main() {
 		cout << "Does Store has WiFi: Yes\n\n";
 	}
 
-	//Store
-	cout << "Business\n";
-	cout << "Business Name: " << item2.getStoreName() << "\n";
-	cout << "Business Revenue: $" << item2.getStoreRevenue() << "\n\n";
+	//----------------------------------------------------------------
+	//Grocery Store
+	cout << "Grocery Store" << "\n";
+	cout << "Name: " << item4.getStoreName() << "\n";
+	cout << "Revenue: " << item4.getStoreRevenue() << "\n";
+	cout << "Physical Address: " << item4.getPhysicalAddress() << "\n";
+
+	//many displays of yes or no just so it looks better
+	//hasWifi
+	if (item4.getHasWifi() == 0) {
+		cout << "WiFi: No\n";
+	}
+	else {
+		cout << "WiFi: Yes\n";
+	}
+	//hasProduce
+	if (item4.getHasProduce() == 0) {
+		cout << "Produce: No\n";
+	}
+	else {
+		cout << "Produce: Yes\n";
+	}
+	//isChain
+	if (item4.getIsChain() == 0) {
+		cout << "Chain: No\n\n";
+	}
+	else {
+		cout << "Chain: Yes\n\n";
+	}
+
 }
-
-//Output the variables of the Grocery Store Class
-
-//Make sure to look at the teacher wanted output so you can change the outputs to the expected output
